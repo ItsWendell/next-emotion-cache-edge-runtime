2fd798996ecfffbb40744ff2e183446f7a5ef780
Wendell Misiedjan <wendellmisiedjan@gmail.com>
2022-08-01 18:39:45 +0200

    minimal reproduceable git repository for cache emotion issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,28 @@
+import createCache from "@emotion/cache";
+import { CacheProvider, ThemeProvider } from "@emotion/react";
+import { GetServerSideProps } from "next";
+
+const cache = createCache({
+  key: "emotion",
+})
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default function Home({ runtime }) {
+  return (
+    <CacheProvider value={cache}>
+      <div>hello from {runtime}</div>
+    </CacheProvider>
+  )
+}
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {
+      runtime: process.env.NEXT_RUNTIME,
+    }
+  }
 }
